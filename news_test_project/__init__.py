00b7a8e804080397a238a39176fb4b9916ba51ec
enovikov <nenomernoi5050@gmail.com>
2020-11-23 15:00:45 +0300

    15-00

---
+++ news_test_project/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'news_test_project.apps.NewsTestConfig'
---
+++ news_test_project/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NewsTestConfig(AppConfig):
+    name = 'news_test_project'
+    verbose_name = "Приложение скрапинг"
