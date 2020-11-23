10fea0cd72aaaf32a5f272fc55c1d6f64b3bd4e9
enovikov <nenomernoi5050@gmail.com>
2020-11-23 14:55:00 +0300

    15-00 23.11

--- news/settings.py
+++ news/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'news_test_project',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
---
+++ news_test_project/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Vacancy
+
+
+def home(request):
+    qs = Vacancy.objects.all()
+    return render(request, 'home.html', {'object_list': qs})
