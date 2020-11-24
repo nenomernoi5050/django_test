edb0359cd621ebd4c0327dbc797d559bf7659c15
enovikov <nenomernoi5050@gmail.com>
2020-11-24 16:09:13 +0300

    24-end-day

--- news_test_project/models.py
+++ news_test_project/models.py
@@ -5,7 +5,7 @@ from news.utils import transliterate
 class City(models.Model):
     name = models.CharField(max_length=50, verbose_name="Название населенного пункта",
                             unique=True)
-    slug = models.CharField(max_length=50, blank=True, verbose_name="URL",unique=True)
+    slug = models.CharField(max_length=50, blank=True, verbose_name="URL", unique=True)
 
     class Meta:
         verbose_name="Название населенного пункта"
--- news_test_project/views.py
+++ news_test_project/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
+
+from .forms import FindForm
 from .models import Vacancy
 
 
 def home(request):
-    qs = Vacancy.objects.all()
-    return render(request, 'home.html', {'object_list': qs})
+    form = FindForm()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+    if city or language:
+        _filter = {}
+        if city:
+            _filter['city__slug'] = city
+        if language:
+            _filter['language__slug'] = language
+        qs = Vacancy.objects.filter(**_filter)
+    return render(request, 'test_project/home.html', {'object_list': qs, 'form': form})
+
+# Поиск по тексту из GET
+# def home(request):
+#     city = request.GET.get('city')
+#     language = request.GET.get('language')
+#     qs = []
+#     if city or language:
+#         _filter = {}
+#         if city:
+#             _filter['city__name'] = city
+#         if language:
+#             _filter['language__name'] = language
+#         qs = Vacancy.objects.filter(**_filter)
+#     return render(request, 'test_project/home.html', {'object_list': qs})
